17d8cdfb959e3f9528b32fc4d5101ab7c05ca71c
Elias <eliasbdeuss@gmail.com>
2018-06-01 11:25:38 -0700

    Added some cards

--- lib/tabs/dashboard.dart
+++ lib/tabs/dashboard.dart
@@ -3,16 +3,55 @@ import 'package:flutter/material.dart';
 class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Container(
-    child:new Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Icon(
-            Icons.menu,
-            size: 150.0,
-            color: Colors.black12
-        ),
-        new Text('Feed Coming Soon!')
-      ],
-    )
-  );
+      child: new ListView(
+        children: <Widget>[
+          new Card(
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: const Icon(Icons.label),
+                  title: const Text('Test Title'),
+                  subtitle: const Text('Test subtitle'),
+                ),
+                new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: new ButtonBar(
+                    children: <Widget>[
+                      new FlatButton(
+                        child: const Text('Test Link'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Card(
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: const Icon(Icons.label),
+                  title: const Text('Test Title'),
+                  subtitle: const Text('Test subtitle'),
+                ),
+                new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: new ButtonBar(
+                    children: <Widget>[
+                      new FlatButton(
+                        child: const Text('Test Link'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      )
+      );
 }
+
--- lib/tabs/tv.dart
+++ lib/tabs/tv.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 class Tv extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Container(
-    child: new Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    child: new ListView(
       children: <Widget>[
         new Card(
           child: new Column(
@@ -13,21 +11,77 @@ class Tv extends StatelessWidget {
             children: <Widget>[
               const ListTile(
                 leading: const Icon(Icons.tv),
-                title: const Text('Redwood TV'),
-                subtitle: const Text('Season 7 Episode 25'),
+                title: const Text('Redwood TV - Season 7 - Episode 25'),
+                subtitle: const Text('Published on May 21, 2018'),
               ),
               new Image.network(
                 'https://img.youtube.com/vi/u-x4LtVkTyQ/maxresdefault.jpg',
               ),
-              new Text('\n*This is currently not working*\n')
+              new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                child: new ButtonBar(
+                  children: <Widget>[
+                    new FlatButton(
+                      child: const Text('Watch Now'),
+                      onPressed: () { /* ... */ },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-
-        )
-
+        ),
+        new Card(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: const Icon(Icons.tv),
+                title: const Text('Redwood TV - Season 7 - Episode 24'),
+                subtitle: const Text('Published on May 12, 2018'),
+              ),
+              new Image.network(
+                'https://img.youtube.com/vi/gZOGIHmLFlQ/hqdefault.jpg',
+              ),
+              new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                child: new ButtonBar(
+                  children: <Widget>[
+                    new FlatButton(
+                      child: const Text('Watch Now'),
+                      onPressed: () { /* ... */ },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        new Card(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: const Icon(Icons.tv),
+                title: const Text('Redwood TV - Season 7 - Episode 23'),
+                subtitle: const Text('Published on May 2, 2018'),
+              ),
+              new Image.network(
+                'https://img.youtube.com/vi/4G84dpt4xKE/maxresdefault.jpg',
+              ),
+              new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                child: new ButtonBar(
+                  children: <Widget>[
+                    new FlatButton(
+                      child: const Text('Watch Now'),
+                      onPressed: () { /* ... */ },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
-
-    ),
+    )
 
   );
 }
