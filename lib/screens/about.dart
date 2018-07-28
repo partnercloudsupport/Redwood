5a42e378ace5fe74f011464bdee1994de7010fb0
Elias <eliasbdeuss@gmail.com>
2018-07-27 18:19:01 -0700

    New NativeAppBar
    Only on some of the pages

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -8,14 +8,10 @@ class About extends StatelessWidget {
   Widget build (BuildContext context) => new Scaffold(
 
     //App Bar
-    appBar: new AppBar(
-      title: new Text(
-        'About', 
-        style: new TextStyle(
-          fontSize: Theme.of(context).platform == TargetPlatform.iOS ? 17.0 : 20.0,
-        ),
-      ),
-      elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+    appBar: NativeAppBar(
+      title: new Text('About'),
+      backgroundColor:
+      Colors.red,
     ),
 
     //Content of tabs
