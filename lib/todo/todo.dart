3f0e6be9241ca9bf2cf33bb51d51054aeee0f3de
Elias <eliasbdeuss@gmail.com>
2018-09-06 11:50:14 -0700

    Getting ready to add sorting to Tasks

--- lib/todo/todo.dart
+++ lib/todo/todo.dart
@@ -92,6 +92,11 @@ class _TodoState extends State<Todo> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               IconButton(
+                icon: Icon(Icons.menu),
+                color: Colors.grey,
+                onPressed: () {},
+              ),
+              IconButton(
                 icon: Icon(Icons.info),
                 color: Colors.grey,
                 onPressed: _showAboutDialog,
